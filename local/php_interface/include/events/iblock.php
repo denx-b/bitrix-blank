f91ef679a86cbe17c1e2aa282e0eafc131c4822f
Denis Bogdanov <info@dbogdanoff.ru>
2017-03-06 22:00:42 +0200

    Добавлен комментарий в файл событий ИБ

--- local/php_interface/include/events/iblock.php
+++ local/php_interface/include/events/iblock.php
@@ -1,5 +1,13 @@
 <?
+/** @global $eventManager */
+
+/*
+ * Вызывается до вставки элемента информационного блока
+ * Может быть использовано для отмены вставки или переопределения некоторых полей.
+ * https://dev.1c-bitrix.ru/api_help/iblock/events/onbeforeiblockelementadd.php
+ */
 $eventManager->addEventHandler('iblock', 'OnBeforeIBlockElementAdd', array('ConsultInfo_Iblock', 'deActiveBeforeAdd'));
+
 class ConsultInfo_Iblock
 {
     /**
@@ -10,7 +18,8 @@ class ConsultInfo_Iblock
     public function deActiveBeforeAdd(&$arParams)
     {
         global $USER;
-        if (!in_array(GROUP_ID_CONTENT, $USER->GetUserGroupArray())) {
+
+        if (!in_array(GROUP_SPEC_ID, $USER->GetUserGroupArray()) && !$USER->IsAdmin()) {
             $arParams['ACTIVE'] = 'N';
         }
     }
