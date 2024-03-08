465a32b4441a247c4a6cd368ee25e8d9fe8d4eae
Denis Bogdanov <info@dbogdanoff.ru>
2024-03-08 20:11:50 +0200

    Copy

--- local/php_interface/include/bootstrap.php
+++ local/php_interface/include/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * The ideology of the framework was developed by Legacy
+ *
+ * https://github.com/denx-b/bitrix-blank/tree/master
+ * https://legacy-agency.ru/
+ */
+
 // autoloader
 require_once __DIR__ . '/autoload.php';
 
