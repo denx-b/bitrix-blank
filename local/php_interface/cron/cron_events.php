e530ad433b5d9d8009531d0680b59dbb2ed41973
Denis Bogdanov <info@dbogdanoff.ru>
2024-03-21 16:15:48 +0200

    cront_events edited

--- local/php_interface/cron/cron_events.php
+++ local/php_interface/cron/cron_events.php
@@ -1,14 +1,18 @@
 <?php
 
 require_once __DIR__ . '/_header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
 
 CAgent::CheckAgents();
-define('BX_CRONTAB_SUPPORT', true);
-define('BX_CRONTAB', true);
-CEvent::CheckEvents();
 
-if (CModule::IncludeModule('subscribe'))
-{
-    $cPosting = new CPosting;
-    $cPosting->AutoSend();
+const BX_CRONTAB_SUPPORT = true;
+const BX_CRONTAB = true;
+
+if (CModule::IncludeModule('sender')) {
+    Bitrix\Sender\MailingManager::checkPeriod(false);
+    Bitrix\Sender\MailingManager::checkSend();
 }
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/tools/backup.php';
+
+CMain::FinalActions();
